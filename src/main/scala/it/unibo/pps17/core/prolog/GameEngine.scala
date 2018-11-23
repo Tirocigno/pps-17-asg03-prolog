a17473822c4d7bdb6faefb5e25b49bae7d5920f4
Tirocigno <fede.naldini1@gmail.com>
2018-11-23 17:57:50 +0100

    Implemented GameEngine trait

--- src/main/scala/it/unibo/pps17/core/prolog/GameEngine.scala
+++ src/main/scala/it/unibo/pps17/core/prolog/GameEngine.scala
@@ -1,14 +1,29 @@
 package it.unibo.pps17.core.prolog
 
+import it.unibo.pps17.core.prolog.wrapper.Position
+
 /**
   * Define the basic operation called inside a game engine.
   */
 trait GameEngine {
 
-  def startGame()
+  /**
+    * Start the game inside the prolog engine.
+    * @return the initial configuration of the game board.
+    */
+  def startGame():List[Position]
 
-  def computeNextGeneration():
 
+  /**
+    * Compute next generation of cells.
+    * @return the initial configuration of the
+    */
+  def computeNextGeneration():List[Position]
+
+  /**
+    * Check if the game is over.
+    * @return true if the game is over, false otherwise.
+    */
   def isGameOver():Boolean
 
 }
