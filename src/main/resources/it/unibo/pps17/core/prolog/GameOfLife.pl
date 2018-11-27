ef986c23aaff256b2dd468db199b977578b61dff
Tirocigno <fede.naldini1@gmail.com>
2018-11-27 12:38:43 +0100

    Prolog computation now working.

--- src/main/resources/it/unibo/pps17/core/prolog/GameOfLife.pl
+++ src/main/resources/it/unibo/pps17/core/prolog/GameOfLife.pl
@@ -2,7 +2,7 @@
 pos(X,Y).
 
 %Fact to represent board dimension.
-board(50,50).
+board(20,20).
 
 %Generation passed since the beginning of the game.
 generation(0).
@@ -17,8 +17,8 @@ alive_cells([]).
 initialize_cell(P,TL,AL) :- random(N,2),set_pos(P,TL,N,AL).
 
 %Initialize every cell inside the board.
-generate_board(50,_, TL, TL) :- !.
-generate_board(X, 50, TL, AL) :- X1 is X + 1,Y1 is 0, generate_board(X1,Y1,TL,AL), !.
+generate_board(X,Y, TL, TL) :- board(X,Y),!.
+generate_board(X, Y, TL, AL) :- board(_,Y), X1 is X + 1,Y1 is 0, generate_board(X1,Y1,TL,AL), !.
 generate_board(X, Y, TL, AL) :- initialize_cell(pos(X,Y),TL,TL2),Y1 is Y+1, generate_board(X,Y1,TL2,AL).
 generate_board(AL) :- generate_board(0,0,[],AL).
 
@@ -45,23 +45,23 @@ XLOW is X+1,XTOP is X-1,YLOW is Y+1,YTOP is Y-1,
 L1=[pos(XLOW,Y), pos(XTOP,Y), pos(X,YLOW), pos(X,YTOP), pos(XLOW, YLOW), pos(XLOW, YTOP), pos(XTOP, YTOP), pos(XTOP,YLOW)],
 delete_out_board(L1,L).
 
-%Compute the value of a cell based on his neighborhood.
-compute_cell(P,TL,TL2) :-  member(P,AL),neighborhood(P,NL), alive_cells(AL),common_elements(NL, AL, C), C >= 2, C <= 3, set_pos(P,TL, 1, TL2),!.
+%Compute the value of a cell based on his neighborhood
+compute_cell(P,TL,TL2) :-  alive_cells(AL),member(P,AL),neighborhood(P,NL),common_elements(NL, AL, C), C >= 2, C =< 3, set_pos(P,TL, 1, TL2),!.
 compute_cell(P,TL,TL2) :- neighborhood(P,NL), alive_cells(AL),common_elements(NL, AL, C), C == 3, set_pos(P,TL, 1, TL2),!.
 compute_cell(_,TL,TL).
 
 %Compute the whole board game on new status.
-compute_board(50,_, TL, TL) :- !.
-compute_board(X, 50, TL, AL) :- X1 is X + 1,Y1 is 0, compute_board(X1,Y1,TL,AL), !.
+
+compute_board(X,Y, TL, TL) :- board(X,Y).
+compute_board(X, Y, TL, AL) :- board(_,Y), X1 is X + 1,Y1 is 0, compute_board(X1,Y1,TL,AL), !.
 compute_board(X, Y, TL, AL) :- compute_cell(pos(X,Y),TL,TL2),Y1 is Y+1, compute_board(X,Y1,TL2,AL).
 compute_board(AL) :- compute_board(0,0,[],AL).
 
+%
+compute_next_generation(AL,G) :- compute_board(AL), set_alive_cells(AL), increase_generation(G).
+
 
 %-----COMMONS------
-
-%Compute the size of a list.
-size([],0).
-size([_|T],M):-size(T,N), M is N+1.
 
 %Append an element in the head of the list.
 append_to_head([],H,[H]):- !.
@@ -82,6 +82,8 @@ common_elements([H|T],L,N) :- common_elements(T,L,N).
 
 %Update the alive cells fact.
 set_alive_cells(AL) :- retract(alive_cells(_)),assert(alive_cells(AL)).
+%Increase the number of generation.
+increase_generation(N) :- generation(X), N is X + 1, retract(generation(_)), assert(generation(N)).
 
 
 
