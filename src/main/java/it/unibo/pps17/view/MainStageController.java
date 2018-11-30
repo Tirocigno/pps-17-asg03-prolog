5767224003712badb9e7444a9240e15ca718d5cb
Tirocigno <fede.naldini1@gmail.com>
2018-11-30 17:01:44 +0100

    Updated to better gui

--- src/main/java/it/unibo/pps17/view/MainStageController.java
+++ src/main/java/it/unibo/pps17/view/MainStageController.java
@@ -1,6 +1,37 @@
 package it.unibo.pps17.view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class MainStageController {
 
+    @FXML
+    public Button startButton;
+
+    @FXML
+    public Button resetButton;
+
+    @FXML
+    public Button nextButton;
+
+    @FXML
+    public Label generation;
+
+    @FXML
+    public Label aliveCells;
+
     public static String MAIN_LAYOUT = "mainStage.fxml";
+
+
+    /**
+     * Initializes the layout.
+     */
+    @FXML
+    public void initialize() {
+        startButton.setOnAction(event -> {
+            generation.setText("Label updated");
+        });
+    }
+
 }
