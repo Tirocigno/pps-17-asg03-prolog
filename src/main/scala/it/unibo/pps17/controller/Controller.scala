5a96c67b7c50bce712126979cd9fd7374485c331
Tirocigno <fede.naldini1@gmail.com>
2018-11-30 18:38:48 +0100

    Splitted controller in trait and implemetation

--- src/main/scala/it/unibo/pps17/controller/Controller.scala
+++ src/main/scala/it/unibo/pps17/controller/Controller.scala
@@ -2,31 +2,55 @@ package it.unibo.pps17.controller
 
 import it.unibo.pps17.core.prolog.GameEngine
 
+trait Controller {
+
+  /**
+    * Reset the game engine.
+    */
+  def resetGameEngine(): Unit
+
+  /**
+    * Get the board dimension.
+    */
+  def boardDimension(): Unit
+
+  /**
+    * Start the game.
+    */
+  def startGame(): Unit
+
+  /**
+    * Compute next generation and print the system status.
+    */
+  def computeNextGeneration(): Unit
+
+}
+
 /**
-  * Controller class of all system.
+  * Textual controller implementation.
   */
-class Controller {
+class TextController() extends Controller {
 
   var gameEngine = GameEngine()
 
   /**
     * Reset the game engine.
     */
-  def resetGameEngine(): Unit = gameEngine = GameEngine()
+  override def resetGameEngine(): Unit = gameEngine = GameEngine()
 
   /**
     * Get the board dimension.
     */
-  def boardDimension(): Unit = println(gameEngine.getBoardDimension)
+  override def boardDimension(): Unit = println(gameEngine.getBoardDimension)
 
   /**
     * Start the game.
     */
-  def startGame(): Unit = println(gameEngine.startGame())
+  override def startGame(): Unit = println(gameEngine.startGame())
 
   /**
     * Compute next generation and print the system status.
     */
-  def computeNextGeneration(): Unit = println(gameEngine.computeNextGeneration())
+  override def computeNextGeneration(): Unit = println(gameEngine.computeNextGeneration())
 
 }
